ee1ce8dcee7ed71c5fa967b8e09d6dd42f10ad55
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-07-26 10:10:27 +0200

    Improved footer content with logo and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
+import { brand } from "@/lib/data/brand";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="p-5 border-t items-center text-sm">
+    <footer className="flex flex-wrap gap-4 justify-between p-10 pt-15 pb-15 border-t items-center text-sm">
       <p>
         &copy; {new Date().getFullYear()} Haiku Coffee. All rights reserved.
       </p>
+
+      <div className="flex flex-col gap-2">
+        <Link href="/account" className="hover:underline">Account</Link>
+        <Link href="/about" className="hover:underline">About Us</Link>
+        <Link href="/about#work-with-us" className="hover:underline">Work with us</Link>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Link href="/transparency" className="hover:underline">Transparency</Link>
+        <Link href="/terms" className="hover:underline">Terms of Service</Link>
+        <Link href="/privacy" className="hover:underline">Privacy Policy</Link>
+      </div>
+
+      <span>{brand.LogoWithText()}</span>
     </footer>
   );
 }
