316f99c02bc1fb750b5eded3f5bbab6f7dc1d0a0
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-07-29 08:23:12 +0200

    improved header logo icon alt text

--- src/components/LogoWithText.tsx
+++ src/components/LogoWithText.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const LogoWithText = () => {
   return (
     <Link href="/" className="flex gap-2 items-center">
-      <Image src="/bean.svg" alt="Haiku Coffee" width={20} height={20} />
+      <Image src="/bean.svg" alt="Haiku Coffee logo" width={20} height={20} />
       <span className="text-lg font-semibold tracking-tight">Haiku Coffee</span>
     </Link>
   );
